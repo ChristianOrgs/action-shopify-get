fa68e4138b6a17266b843325c9a195c3caf7b543
Christian Orgs <c.orgs@me.com>
2020-07-27 17:34:10 +0200

    fix: fixed config overwrite development ID

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-theme get --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH $INPUT_ARGS
+theme get --password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH $INPUT_ARGS -e production
